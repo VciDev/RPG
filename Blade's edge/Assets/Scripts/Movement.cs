6934a050a68f6684bd3a7b85bce72e9e621a2263
Ryan <randomvendetta@hotmail.com>
2016-10-30 16:18:05 +1100

    First Commit
    Commiting game to Github

---
+++ Blade's edge/Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float rotationSpeed = 1f;
+    public float thrustSpeed = 1f;
+
+    public void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, 0, rotationSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0, -rotationSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector2 angle = new Vector2(-Mathf.Sin(transform.rotation.eulerAngles.z * Mathf.Deg2Rad), Mathf.Cos(transform.rotation.eulerAngles.z * Mathf.Deg2Rad));
+            GetComponent<Rigidbody2D>().AddForce(angle * thrustSpeed);
+        }
+    }
+}
